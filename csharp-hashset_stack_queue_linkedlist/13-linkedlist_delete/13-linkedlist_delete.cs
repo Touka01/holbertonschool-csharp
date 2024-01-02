3cae1fde06b8518674b8f699ffe4a5f88085182a
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2024-01-02 19:08:22 +0100

    reset

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,20 @@
+﻿// a method that deletes the node at given position in a LinkedList.
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int idx = 0;
+        foreach(var elem in myLList)
+        {
+            if(idx == index)
+            {
+                myLList.Remove(elem);
+                return;
+            }
+            idx++;
+        }
+    }
+}
